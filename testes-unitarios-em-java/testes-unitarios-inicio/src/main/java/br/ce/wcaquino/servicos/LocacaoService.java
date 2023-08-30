cc017c0fdaba179eb23f0a0caaf7a231e44dfc9b
Marlene Moraes <marlenevmoraes@outlook.com>
2023-08-29 21:42:23 -0300

    junit

--- testes-unitarios-em-java/testes-unitarios-inicio/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ testes-unitarios-em-java/testes-unitarios-inicio/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -4,6 +4,9 @@ import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
 
 import java.util.Date;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
@@ -29,7 +32,8 @@ public class LocacaoService {
 		return locacao;
 	}
 
-	public static void main(String[] args) {
+	@Test
+	public void LocacaoServiceTest() {
 		// Montagem do cenario - instancia de classes
 		LocacaoService servico = new LocacaoService();
 
@@ -39,10 +43,10 @@ public class LocacaoService {
 		// Acoes a serem realizadas
 		Locacao locacao = servico.alugarFilme(usuario, filme);
 
-		// Verificacao das etapas
-		System.out.println(locacao.getValor() == 5.50);
-		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+		// Verificacao das etapas (de forma auto-verificavel)
+		Assert.assertTrue(locacao.getValor() == 5.50);
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 
 	}
 }
