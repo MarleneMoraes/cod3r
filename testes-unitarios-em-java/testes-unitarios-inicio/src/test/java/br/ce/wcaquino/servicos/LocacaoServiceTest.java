096e839b8d04f402ad93658a425ac6b90d522253
Marlene Moraes <marlenevmoraes@outlook.com>
2024-01-04 21:54:11 -0300

    before e after

--- testes-unitarios-em-java/testes-unitarios-inicio/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ testes-unitarios-em-java/testes-unitarios-inicio/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -7,6 +7,7 @@ import static org.hamcrest.CoreMatchers.is;
 import java.util.Date;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
@@ -19,29 +20,33 @@ import br.ce.wcaquino.exceptions.FilmesSemEstoqueException;
 import br.ce.wcaquino.exceptions.LocadoraException;
 
 public class LocacaoServiceTest {
+	private LocacaoService servico;
+	
 	@Rule
 	public ErrorCollector error = new ErrorCollector();
 	
 	@Rule
 	public ExpectedException exception = ExpectedException.none();
+	
+	@Before
+	public void setup() {
+		servico = new LocacaoService();		
+	}
 
 	@Test
 	public void test() throws Exception {
-		LocacaoService servico = new LocacaoService();
 		Usuario usuario = new Usuario("Maria");
 		Filme filme = new Filme("Grease", 2, 5.50);
 
-		Locacao locacao;
-		locacao = servico.alugarFilme(usuario, filme);
+		Locacao locacao = servico.alugarFilme(usuario, filme);
 
 		error.checkThat(locacao.getValor(), is(equalTo(5.50)));
 		error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
 		error.checkThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
 	}
 	
-	@Test(expected=FilmesSemEstoqueException.class) // teste executado com seguranca por garantir que somente esta excessao sera lancada
+	@Test(expected=FilmesSemEstoqueException.class) 
 	public void testLocacao_filmeSemEstoque() throws Exception {
-		LocacaoService servico = new LocacaoService();
 		Usuario usuario = new Usuario("Joana");
 		Filme filme = new Filme("Grease", 0, 5.50);
 
@@ -51,7 +56,6 @@ public class LocacaoServiceTest {
 	
 	@Test
 	public void testLocacao_usuarioVazio() throws FilmesSemEstoqueException {
-		LocacaoService servico = new LocacaoService();
 		Filme filme = new Filme("Grease", 2, 5.50);
 
 		try {
@@ -66,7 +70,6 @@ public class LocacaoServiceTest {
 	
 	@Test
 	public void testLocacao_filmeVazio() throws FilmesSemEstoqueException, LocadoraException {
-		LocacaoService servico = new LocacaoService();
 		Usuario usuario = new Usuario("Maria");
 
 		exception.expect(LocadoraException.class);
