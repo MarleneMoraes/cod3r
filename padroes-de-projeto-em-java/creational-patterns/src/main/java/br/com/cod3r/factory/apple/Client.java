d8d04765e7639a3899420edc6fb61ef53e4fcb8a
Marlene Moraes <marlenevmoraes@outlook.com>
2024-02-12 22:12:24 -0300

    factory method

--- padroes-de-projeto-em-java/creational-patterns/src/main/java/br/com/cod3r/factory/apple/Client.java
+++ padroes-de-projeto-em-java/creational-patterns/src/main/java/br/com/cod3r/factory/apple/Client.java
@@ -1,11 +1,18 @@
 package br.com.cod3r.factory.apple;
 
+import br.com.cod3r.factory.apple.factory.IPhoneFactory;
+import br.com.cod3r.factory.apple.model.IPhone;
+
 public class Client {
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("### Ordering an iPhone X");
-		
+		IPhone iphoneX = IPhoneFactory.orderIPhone("X", "standard");
+		System.out.println(iphoneX);
+
 		System.out.println("\n\n### Ordering an iPhone 11 HighEnd");
+		IPhone iphone11Pro = IPhoneFactory.orderIPhone("11", "highEnd");
+		System.out.println(iphone11Pro);
 	}
 }
